3b8276e7dce7a59ce9d2fb7c51527eb0a5dbcf7e
Dorian Yeh <56494806+civond@users.noreply.github.com>
2021-04-08 02:44:40 -0700

    Add files via upload

--- figures/createPerformance_graphs.py
+++ figures/createPerformance_graphs.py
@@ -1,47 +1,38 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd;import matplotlib.pyplot as plt;
 
 def importData(): #note, clean up the notes.txt files
     rawFMON_data = 'https://raw.githubusercontent.com/civond/FMON_DataManagement/main/FMON_performanceData.csv';
     df = pd.read_csv(rawFMON_data);
     df['trialPercentage'] = df['totalCorrect'] / df['totalAttempts'];
-    df[['odorant', 'concentration']] = df.odorantConc.str.split(' ', expand=True)
-
     df.drop(df.columns.difference([
         'ID',
         'experimentType',
-        'odorant',
+        'odor',
         'concentration',
         'trial',
         'trialPercentage',
-    ]),
-        1, inplace=True);
-    df['odorant'] = df['odorant'].replace(['pinene(7)'], 'pinene');
-    df['odorant'] = df['odorant'].replace(['2-pe','2-PE'], '2pe');
-    df['odorant'] = df['odorant'].replace(['vanillin(7)','Vanillin'], 'vanillin');
-    df['odorant'] = df['odorant'].replace(['Benzaldehyde'], 'benzaldehyde');
+    ]),1, inplace=True);
     fig, ax = plt.subplots()
-
     df100 = df[df['experimentType'] == '100-0'];
-    #df100_pinene = df100[df100['odorant'] == 'pinene'];
-    #df100_pinene.groupby('ID').plot(x='trial', y='trialPercentage', ax=ax, legend=False);
+    for mouseID in df100.ID.unique():
+        df100_mouseSplit = df100[df100['ID'] == mouseID]
+        fileTitle = 'figures/' + str(mouseID)+'_100Performance.png'
+        for odor in df100_mouseSplit.odor.unique():
+            odorSplit = df100_mouseSplit[df100_mouseSplit['odor'] == odor]
+            if odor == '2pe':
+                odorSplit.plot(x='trial', y='trialPercentage', ax=ax, legend=False,marker='o',color='purple');
+            elif odor == 'pinene':
+                odorSplit.plot(x='trial', y='trialPercentage', ax=ax, legend=False, marker='s', color='green');
+            elif odor == 'vanillin':
+                odorSplit.plot(x='trial', y='trialPercentage', ax=ax, legend=False, marker='D', color='magenta');
+            elif odor == 'benzaldehyde':
+                odorSplit.plot(x='trial', y='trialPercentage', ax=ax, legend=False, marker='p', color='red');
+        #df100_mouseSplit.groupby('odor').plot(x='trial', y='trialPercentage', ax=ax, legend=False,marker='o');
+        plt.title('{} 100-0 Performance'.format(mouseID));
+        plt.xlabel('Session', fontsize=12);plt.ylabel('Percentage Correct', fontsize=12)
+        plt.legend(df100_mouseSplit.odor.unique())
+        plt.savefig(fileTitle);plt.cla();
 
-    #df100_2pe = df100[df100['odorant'] == '2pe'];
-    #df100_2pe.groupby('ID').plot(x='trial', y='trialPercentage', ax=ax, legend=False);
-
-    #df100_vanillin = df100[df100['odorant'] == 'vanillin'];
-    #df100_vanillin.groupby('ID').plot(x='trial', y='trialPercentage', ax=ax, legend=False);
-
-    df100.groupby('ID').plot(x='trial', y='trialPercentage', ax=ax, legend=False);
-
-
-    #plt.legend(['ID'])
-    plt.xlabel("Session")
-    plt.ylabel("Percentage Correct")
-    plt.title("100-0 Overall Performance")
-    #ax.set_xlim(right=35)
-
-    plt.show()
+    #plt.show()
 
 importData()
