bddeb4404d04004c2bad3d1eec0bdc791b33c61d
Dorian Yeh <56494806+civond@users.noreply.github.com>
2021-04-13 22:01:33 -0700

    Add files via upload

--- figures/createPerformance_graphs.py
+++ figures/createPerformance_graphs.py
@@ -1,19 +1,23 @@
-import pandas as pd;import matplotlib.pyplot as plt;
+import pandas as pd
+import matplotlib.pyplot as plt;import matplotlib.ticker as ticker;
 
 def importData(): #note, clean up the notes.txt files
     rawFMON_data = 'https://raw.githubusercontent.com/civond/FMON_DataManagement/main/FMON_performanceData.csv';
     df = pd.read_csv(rawFMON_data);
     df['trialPercentage'] = df['totalCorrect'] / df['totalAttempts'];
     df.drop(df.columns.difference([
-        'ID',
-        'experimentType',
+        'ID','experimentType',
         'odor',
         'concentration',
         'trial',
         'trialPercentage',
-    ]),1, inplace=True);
+    ]),1,
+        inplace=True);
+
     fig, ax = plt.subplots()
     df100 = df[df['experimentType'] == '100-0'];
+    #fig = plt.subplots(2,2)
+
     for mouseID in df100.ID.unique():
         df100_mouseSplit = df100[df100['ID'] == mouseID]
         fileTitle = 'figures/' + str(mouseID)+'_100Performance.png'
@@ -27,12 +31,13 @@ def importData(): #note, clean up the notes.txt files
                 odorSplit.plot(x='trial', y='trialPercentage', ax=ax, legend=False, marker='D', color='magenta');
             elif odor == 'benzaldehyde':
                 odorSplit.plot(x='trial', y='trialPercentage', ax=ax, legend=False, marker='p', color='red');
-        #df100_mouseSplit.groupby('odor').plot(x='trial', y='trialPercentage', ax=ax, legend=False,marker='o');
+        for axis in [ax.xaxis]:
+            axis.set_major_locator(ticker.MaxNLocator(integer=True))
+
         plt.title('{} 100-0 Performance'.format(mouseID));
         plt.xlabel('Session', fontsize=12);plt.ylabel('Percentage Correct', fontsize=12)
         plt.legend(df100_mouseSplit.odor.unique())
         plt.savefig(fileTitle);plt.cla();
 
-    #plt.show()
 
 importData()
